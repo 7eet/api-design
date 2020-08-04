311d6cd27fbe2af14f9f210d987d38833d033db8
7eet <raisimranjeetsingh@gmail.com>
2020-08-04 21:30:00 +0530

    added server file

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+var path = require("path");
+var bodyParser = require("body-parser");
+const port = 7000;
+
+var indexRouter = require("./routes/index");
+
+// add static files support
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(bodyParser.json());
+
+app.use("/", indexRouter);
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
